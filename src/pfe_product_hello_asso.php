aa24d1b67652ec16d7a21bfdee83130df2601671
Kevin Janiky <kevin.janiky@gmail.com>
2024-12-03 09:29:03 +0100

    base PHP

---
+++ src/pfe_product_hello_asso.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: PFE - Product Hello Asso
+ * Description: Create Cpt for product & hello asso integration
+ * Version: 1.0.0
+ * Author: AtomikAgency
+ * Author URI: https://atomikagency.fr/
+ */
+
+define('PFE_PRODUCT_HELLO_ASSO_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('PFE_PRODUCT_HELLO_ASSO_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+if (!defined('ABSPATH')) {
+    exit; // Prevent direct access
+}
+
+require_once PFE_PRODUCT_HELLO_ASSO_PLUGIN_DIR . 'update-checker.php';
